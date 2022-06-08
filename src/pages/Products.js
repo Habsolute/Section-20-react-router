b07baeeed12ef6f14bbb4c2ecb6d0f519cc49f70
Habsolute <85709216+Habsolute@users.noreply.github.com>
2022-06-08 11:36:42 -0400

    Redirecting User

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,20 @@
 // import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Products() {
   return (
     <section>
       <h1>The Products page</h1>
       <ul>
-        <li>Book</li>
-        <li>Carpet</li>
-        <li>Online course</li>
+        <li>
+          <Link to={"/products/p1"}>Book</Link>
+        </li>
+        <li>
+          <Link to={"/products/p2"}>Carpet</Link>
+        </li>
+        <li>
+          <Link to={"/products/p3"}>Online course</Link>
+        </li>
       </ul>
     </section>
   );
